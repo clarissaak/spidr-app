5649a922aa57b319f27a443087c3f5d94fc91235
Clarissa Kristanto <clarissaakristanto@gmail.com>
2025-07-15 11:31:31 -0700

    Basic Form Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, {useEffect, useState} from "react";
 import Particles, {initParticlesEngine} from "@tsparticles/react";
 import {loadFull} from "tsparticles";
-import logo from "./logo.svg";
 import "./App.css";
 import particlesOptions from "./particles.json";
+import spidr from "./spidr.png";
+
+// custom input
+function MyInput({label, id, value, onChange}) {
+    return (
+      <div className="MyInput">
+        <label>{label}:
+          <input type="text" id={id} value={value} onChange={onChange}/>
+        </label>
+      </div>
+    )
+}
 
 function App() {
     const [init, setInit] = useState(false);
@@ -20,35 +31,47 @@ function App() {
         });
     }, []);
 
+     // input array
+     const [formData, setFormData] = useState({
+        fname: '',
+        lname: '',
+        phone: '',
+        email: '',
+        cost: '',
+        pin: '',
+    });
+
+    // updates form data if any changes are made before submitting
+    const handleChange = (key) => (event) => {
+        setFormData({ ...formData, [key]: event.target.value});
+    };
+
+    // prints data to console
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+
+
+
     return (
         <div className="App">
             {init && <Particles options={particlesOptions}/>}
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save
-                    to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
+                <img className="Spidr" src={spidr} alt="spidr"/>
             </header>
+            <div className="FormContainer">
+                <header className="Form-header">Please complete the following interest form: </header>
+                <form className="Form" onSubmit={onSubmit}>
+                    <MyInput label="First Name" id="fname" value={formData.fname} onChange={handleChange('fname')}/>
+                    <MyInput label="Last Name" id="lname" value={formData.lname} onChange={handleChange('lname')} />
+                    <MyInput label="Phone Number" id="phone" value={formData.phone} onChange={handleChange('phone')} />
+                    <MyInput label="Email" id="email" value={formData.email} onChange={handleChange('email')} />
+                    <MyInput label="Guess our air fryer's cost" id="cost" value={formData.cost} onChange={handleChange('cost')} />
+                    <MyInput label="Spidr Pin" id="pin" value={formData.pin} onChange={handleChange('pin')} />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     );
 }
